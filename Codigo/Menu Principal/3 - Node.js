f79cd333775a3a6cfe82f590d9b85d4ebc86a47d
Gabriel Caldeira <138264512+GaCaldeira@users.noreply.github.com>
2024-09-05 16:32:15 -0300

    pagina html do menu principal esta completa, e a pagina css falta apenas adicionar a imagem de fundo. estou resolvendo o link entre as paginas via javascript

--- Codigo/Menu Principal/3 - Node.js
+++ Codigo/Menu Principal/3 - Node.js
@@ -1,1 +1,11 @@
+function redirectToContinue() {
+    window.location.assign('./Continue/1 - Index.html');
+}
 
+function redirectToNewGame() {
+    window.location.assign('./New Game/1 - Index.html'); 
+}
+
+document.getElementById("options").onclick = function() {
+    window.location.href = "1 - Index.html";
+};
